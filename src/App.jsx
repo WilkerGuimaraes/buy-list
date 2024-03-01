c256c51454d9ce401ee4648d4ca902c3844b89d1
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-01 11:21:21 -0300

    feat: Add product list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 import { AddButton, AddInput, Container } from "./App.style";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      id: "1",
+      name: "Milk",
+    },
+    {
+      id: "2",
+      name: "noodle",
+    },
+  ]);
+
   return (
     <Container>
       <h1>Buy List</h1>
       <AddInput type="text" placeholder="product..." />
       <AddButton>Add</AddButton>
+
+      {products.map((product) => (
+        <p>{product.name}</p>
+      ))}
     </Container>
   );
 }
